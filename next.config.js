72c9e1ea348bcb97545cf130eaefc2fa1ae0cada
yesyousuppur-source <yesyousuppur@gmail.com>
2026-04-03 12:16:52 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
